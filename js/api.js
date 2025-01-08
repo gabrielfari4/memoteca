b0c8e2d4617ac7e65f984b0b0d2964c09747e4a0
gabrielfari4 <gabrielfaria88@gmail.com>
2025-01-08 13:38:26 -0300

    :recycle: refactor: implementando axios no lugar do fetch

--- js/api.js
+++ js/api.js
@@ -3,8 +3,10 @@ const URL_BASE = 'http://localhost:3000';
 const api = {
     async buscarPensamentos() {
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos`)
-            return await response.json();
+            /* const response = await fetch(`${URL_BASE}/pensamentos`)
+            return await response.json(); */
+            const response = await axios.get(`${URL_BASE}/pensamentos`)
+            return await response.data
         } catch (error) {
             alert("Houve um erro ao buscar pensamentos: ", error);
             throw error;
@@ -13,14 +15,16 @@ const api = {
 
     async salvarPensamento(pensamento) {
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos`,  {
+            /* const response = await fetch(`${URL_BASE}/pensamentos`,  {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(pensamento)
-            })
-            return await response.json();
+            }) 
+            return await response.json(); */
+            const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento)
+            return await response.data;
         } catch (error) {
             alert("Houve um erro ao salvar pensamento: ", error);
             throw error;
@@ -29,8 +33,10 @@ const api = {
 
     async buscarPensamentoPorId(id) {
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
-            return await response.json();
+            /* const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
+            return await response.json(); */
+            const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
+            return await response.data;
         } catch (error) {
             alert("Houve um erro ao buscar pensamento: ", error);
             throw error;
@@ -39,14 +45,16 @@ const api = {
 
     async editarPensamento(pensamento) {
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`,  {
+            /* const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`,  {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(pensamento)
             })
-            return await response.json();
+            return await response.json(); */
+            const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento)
+            return await response.data;
         } catch (error) {
             alert("Houve um erro ao editar o pensamento: ", error);
             throw error;
@@ -55,9 +63,10 @@ const api = {
 
     async excluirPensamento(id) {
         try {
-            const response = await fetch(`${URL_BASE}/pensamentos/${id}`,  {
+            /* const response = await fetch(`${URL_BASE}/pensamentos/${id}`,  {
                 method: "DELETE",
-            })
+            }) */
+            const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`)
         } catch (error) {
             alert("Houve um erro ao excluir o pensamento: ", error);
             throw error;
--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,13 @@
 import api from "./api.js";
 
 const ui = {
-
     async preencherFormulario(pensamentoId) {
         const pensamento = await api.buscarPensamentoPorId(pensamentoId);
-        document.getElementById('pensamento-id').value = pensamento.id;
-        document.getElementById('pensamento-conteudo').value = pensamento.conteudo;
-        document.getElementById('pensamento-autoria').value = pensamento.autoria;
+        document.getElementById("pensamento-id").value = pensamento.id;
+        document.getElementById("pensamento-conteudo").value =
+            pensamento.conteudo;
+        document.getElementById("pensamento-autoria").value =
+            pensamento.autoria;
     },
 
     async renderizarPensamentos() {
@@ -17,7 +18,7 @@ const ui = {
             const pensamentos = await api.buscarPensamentos();
             pensamentos.forEach(ui.adicionarPensamento);
             if (pensamentos.length === 0) {
-                this.listaVazia()
+                this.listaVazia();
             }
         } catch (error) {
             alert("Erro: ", error);
@@ -26,87 +27,88 @@ const ui = {
 
     adicionarPensamento(pensamento) {
         const listaPensamentos = document.getElementById("lista-pensamentos");
-        const li = document.createElement('li')
-        li.setAttribute('data-id', pensamento.id)
-        li.classList.add('li-pensamento')
+        const li = document.createElement("li");
+        li.setAttribute("data-id", pensamento.id);
+        li.classList.add("li-pensamento");
 
-        const iconeAspas = document.createElement('img')
+        const iconeAspas = document.createElement("img");
         iconeAspas.src = "assets/imagens/aspas-azuis.png";
-        iconeAspas.alt = "Aspas azuis"
-        iconeAspas.classList.add('icone-aspas')
+        iconeAspas.alt = "Aspas azuis";
+        iconeAspas.classList.add("icone-aspas");
 
-        const texto = document.createElement("div")
-        texto.textContent = pensamento.conteudo
-        texto.classList.add('pensamento-conteudo')
+        const texto = document.createElement("div");
+        texto.textContent = pensamento.conteudo;
+        texto.classList.add("pensamento-conteudo");
 
-        const autoria = document.createElement("div")
-        autoria.textContent = pensamento.autoria
-        autoria.classList.add('pensamento-autoria')
+        const autoria = document.createElement("div");
+        autoria.textContent = pensamento.autoria;
+        autoria.classList.add("pensamento-autoria");
 
-        const botaoEditar = document.createElement("button")
-        botaoEditar.classList.add('botao-editar')
-        botaoEditar.addEventListener('click', () => {
-            ui.preencherFormulario(pensamento.id)
-        })
+        const botaoEditar = document.createElement("button");
+        botaoEditar.classList.add("botao-editar");
+        botaoEditar.addEventListener("click", () => {
+            ui.preencherFormulario(pensamento.id);
+        });
 
-        const iconeEditar = document.createElement('img')
-        iconeEditar.src = "assets/imagens/icone-editar.png"
-        iconeEditar.alt = "Editar"
-        botaoEditar.appendChild(iconeEditar)
+        const iconeEditar = document.createElement("img");
+        iconeEditar.src = "assets/imagens/icone-editar.png";
+        iconeEditar.alt = "Editar";
+        botaoEditar.appendChild(iconeEditar);
 
-        const botaoExcluir = document.createElement("button")
-        botaoExcluir.classList.add('botao-excluir')
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.classList.add("botao-excluir");
         botaoExcluir.onclick = async () => {
             try {
-                await api.excluirPensamento(pensamento.id)
-                ui.renderizarPensamentos()
+                await api.excluirPensamento(pensamento.id);
+                ui.renderizarPensamentos();
             } catch (error) {
-                alert("Erro ao excluir pensamento.")
+                alert("Erro ao excluir pensamento.");
             }
-        }
+        };
 
-        const iconeExcluir = document.createElement('img')
-        iconeExcluir.src = "assets/imagens/icone-excluir.png"
-        iconeExcluir.alt = "Excluir"
-        botaoExcluir.appendChild(iconeExcluir)
+        const iconeExcluir = document.createElement("img");
+        iconeExcluir.src = "assets/imagens/icone-excluir.png";
+        iconeExcluir.alt = "Excluir";
+        botaoExcluir.appendChild(iconeExcluir);
 
-        const icones = document.createElement('div')
-        icones.classList.add("icones")
-        icones.appendChild(botaoEditar)
-        icones.appendChild(botaoExcluir)
+        const icones = document.createElement("div");
+        icones.classList.add("icones");
+        icones.appendChild(botaoEditar);
+        icones.appendChild(botaoExcluir);
 
-        li.appendChild(iconeAspas)
-        li.appendChild(texto)
-        li.appendChild(autoria)
-        li.appendChild(icones)
-        listaPensamentos.appendChild(li)
+        li.appendChild(iconeAspas);
+        li.appendChild(texto);
+        li.appendChild(autoria);
+        li.appendChild(icones);
+        listaPensamentos.appendChild(li);
     },
-    
+
     limparFormulario() {
-        document.getElementById('pensamento-form').reset();
+        document.getElementById("pensamento-form").reset();
     },
 
     listaVazia() {
-        const listaContainer = document.getElementById("lista-pensamentos-container");
+        const listaContainer = document.getElementById(
+            "lista-pensamentos-container"
+        );
 
-            const texto = document.createElement('p')
-            texto.id = 'texto-vazio'
-            texto.textContent = "Nada por aqui ainda, que tal compartilhar alguma ideia?"
-    
-            const imagem = document.createElement('img')
-            imagem.id = 'imagem-vazio'
-            imagem.src = "assets/imagens/lista-vazia.png"
-            imagem.alt = "Ícone caixa vazia"
-    
-            const vazio = document.createElement('div')
-            vazio.appendChild(texto)
-            vazio.appendChild(imagem)
-            vazio.style.display = 'flex'
-            vazio.style.flexDirection = 'column'
-    
-            listaContainer.appendChild(vazio)
-        
-    }
+        const texto = document.createElement("p");
+        texto.id = "texto-vazio";
+        texto.textContent =
+            "Nada por aqui ainda, que tal compartilhar alguma ideia?";
+
+        const imagem = document.createElement("img");
+        imagem.id = "imagem-vazio";
+        imagem.src = "assets/imagens/lista-vazia.png";
+        imagem.alt = "Ícone caixa vazia";
+
+        const vazio = document.createElement("div");
+        vazio.classList.add("vazio-div");
+        vazio.appendChild(texto);
+        vazio.appendChild(imagem);
+
+        listaContainer.appendChild(vazio);
+    },
 };
 
 export default ui;
